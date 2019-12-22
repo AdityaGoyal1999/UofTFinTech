4060b99f0d43dcf9ce8f4dd3489a3e6978b3aee2
adityagoyal <goyaladi>
2019-12-22 16:49:34 -0500

    Reordered the directories

---
+++ templates/src/js/user.js
@@ -0,0 +1,96 @@
+function initUserAuth(data){
+    // Initialize Firebase
+    <!-- The core Firebase JS SDK is always required and must be listed first -->
+// <script src="https://www.gstatic.com/firebasejs/7.6.1/firebase-app.js"></script>
+
+        // <!-- TODO: Add SDKs for Firebase products that you want to use
+    // https://firebase.google.com/docs/web/setup#available-libraries -->
+    //     <script src="https://www.gstatic.com/firebasejs/7.6.1/firebase-analytics.js"></script>
+
+        // <script>
+    // Your web app's Firebase configuration
+    var firebaseConfig = {
+        apiKey: "AIzaSyBhn1G5-Zk08AZ435rUMIkeXmf1upKcmeI",
+        authDomain: "kabootar-74acc.firebaseapp.com",
+        databaseURL: "https://kabootar-74acc.firebaseio.com",
+        projectId: "kabootar-74acc",
+        storageBucket: "kabootar-74acc.appspot.com",
+        messagingSenderId: "255478711946",
+        appId: "1:255478711946:web:6a805b498fc103aea14f73",
+        measurementId: "G-MPJ82G8EVZ"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+
+
+    data.provider = new firebase.auth.GoogleAuthProvider();
+
+    $("#firebaseTest").off('click').on('click', function () {
+        if(data.loggingIn)
+            return;
+        googleSignIn(data);
+    });
+
+    firebase.auth().onAuthStateChanged(function (firebaseUser) {
+        if(firebaseUser) {
+            data.user = firebaseUser;
+            showUser(data);
+            fixUI();
+            if(!data.disconnected) {
+                data.loggingIn = true;
+            }
+        }else{
+            // Update the sign-in button to Google sign-in button
+            data.user = null;
+            data.loggingIn = false;
+
+            $("#firebaseTest").off('click').on('click', function () {
+                if(data.loggingIn){return;}
+                googleSignIn(data);
+            });
+            //$("#firebaseTest").html("<img src=\"assets/google/btn_google_signin_dark_normal_web@2x.png\" width=\"191\" />");
+            fixUI();
+        }
+    });
+}
+
+function showUser(data){
+    $("#userHolder").html("<div class=\"media\">\n" +
+        "  <img style=\"width:64px\" src=\"" + data.user.photoURL + "\" class=\"mr-3\" alt=\"...\">\n" +
+        "  <div class=\"media-body\">\n" +
+        "    <h5 class=\"mt-0\">" + data.user.displayName + "</h5>\n" +
+        "    \n" +
+        "  </div>\n" +
+        "</div>");
+}
+
+function googleSignIn(data){
+    if(data.disconnected){
+        console.log("can't log in: disconnected from socket server")
+        fixUI();
+    }else{
+        data.loggingIn = true;
+        firebase.auth().signInWithPopup(data.provider).then(function (result) {
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            var token = result.credential.accessToken;
+            // The signed-in user info.
+            //result.user;
+
+        }).catch(function (error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            console.log(errorCode);
+            console.log(errorMessage);
+            // The email of the user's account used.
+
+            data.loggingIn = false;
+        });
+    }
+}
+
+function googleSignOut(data){
+    firebase.auth().signOut();
+    data.user = null;
+}
