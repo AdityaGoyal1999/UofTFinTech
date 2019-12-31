aacbd0f3955a946f41f160c098af3a59217549c2
AdityaGoyal1999 <44474678+AdityaGoyal1999@users.noreply.github.com>
2019-12-31 13:23:31 -0500

    Add files via upload
    Features added :-
    - Created the Firebase Database
    - Firebase Database stores the information about the user that is given by google's account
    - Separate files maintained for database.js, login.js, user.js

---
+++ UofTFinTech/README.md
@@ -0,0 +1,8 @@
+# UofTFinTech
+
+<p> We are planning to make an international transaction system that is community driven. <br>
+<br> The process is based on the famous escrow system but in this approach, we are not being the third party
+<br> For this project, we will be using the google firebase's cloud service to store data temporarily. Later, we will 
+develope our own database for added security of the users.<p>
+<p> For the added security of the users, we are planning to have reCAPTCHA for our login page.</p>
+<p> We are using google's firebase to maintain authentication mainiting the database. The database that we are using is based on NoSQL which is scalable and addition of new properties is possible at any time. </p>
---
+++ UofTFinTech/templates/src/js/user.js
@@ -0,0 +1,29 @@
+
+
+$(function() {
+    initializeFirebaseApp();
+
+    firebase.auth().onAuthStateChanged(function (user) {
+        console.log(user);
+        if (user) {
+            var displayName = user.displayName;
+            var email = user.email;
+            // could use this to check if the user has logged in successfully and then load his entry in the database
+            var emailVerified = user.emailVerified;
+            var photoURL = user.photoURL;
+            var isAnonymous = user.isAnonymous;
+            var uid = user.uid;
+            var providerData = user.providerData;
+
+            saveData(email, displayName, uid);
+
+            $("#profilePic").attr("src", photoURL);
+            $("#usernameHeader").html(displayName);
+            $("#locationParagraph").html(email);
+
+        } else {
+            // User is signed out.
+            // ...
+        }
+    });
+});
