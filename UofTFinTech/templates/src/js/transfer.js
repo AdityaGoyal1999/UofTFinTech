e680074b8d36e07c82bbb6d1f254422113968a02
AdityaGoyal1999 <44474678+AdityaGoyal1999@users.noreply.github.com>
2020-01-03 18:57:09 -0500

    Add files via upload
    Buttons added and new file to show transactions created

---
+++ UofTFinTech/templates/src/js/transfer.js
@@ -0,0 +1,8 @@
+function saveRequest()
+{
+    // window.alert("getting here");
+    var country = document.getElementById("country").innerHTML;
+    var amount = document.getElementById("amount").innerHTML;
+    var flexibility = document.getElementById("flexibility").innerHTML;
+    var deadine = document.getElementById("deadline").innerHTML;
+}
