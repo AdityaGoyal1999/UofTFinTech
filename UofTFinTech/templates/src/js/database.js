4f85c364d4773e6491bc7789cfaff02e006c4c7a
AdityaGoyal1999 <44474678+AdityaGoyal1999@users.noreply.github.com>
2020-01-02 22:03:24 -0500

    Add files via upload

---
+++ UofTFinTech/templates/src/js/database.js
@@ -0,0 +1,12 @@
+function saveData(email, name, uid)
+{
+    var firebaseRef = firebase.database().ref();
+
+    firebaseRef.child(uid+"/email").set(email);
+    firebaseRef.child(uid+"/name").set(name);
+    firebaseRef.child(uid+"/ratings").set(0);
+    firebaseRef.child(uid+"/request").set(0);
+    firebaseRef.child(uid+"/request/location").set(0);
+    firebaseRef.child(uid+"request/time").set(0);
+    firebaseRef.child(uid+"request/flexibility").set(0);
+}
---
+++ UofTFinTech/templates/src/js/firebaseConfig.js
@@ -0,0 +1,15 @@
+function initializeFirebaseApp()
+{
+    var firebaseConfig = {
+        apiKey: "AIzaSyCCRMtDs_u5jd9Pph2cIa7O2Af8MPtOjnY",
+        authDomain: "uoftfint.firebaseapp.com",
+        databaseURL: "https://uoftfint.firebaseio.com",
+        projectId: "uoftfint",
+        storageBucket: "uoftfint.appspot.com",
+        messagingSenderId: "284130133968",
+        appId: "1:284130133968:web:0c9b353bf4a26bfb941bd5",
+        measurementId: "G-8PZR1XMMQV"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+}
