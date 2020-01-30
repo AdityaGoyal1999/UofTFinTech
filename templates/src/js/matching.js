beb7d7a9aba8f117809742f0f50fe3edeb2fa9fd
glowies <glowies@glowies-mp.local>
2020-01-30 14:40:43 -0500

    Displaying transactions properly.

--- templates/src/js/matching.js
+++ templates/src/js/matching.js
@@ -1,64 +1,7 @@
-// $(function(){
-//     initializeFirebaseApp();
-//
-//     let firebaseRef = firebase.database().ref();
-//     firebaseRef.once('value').then(function(snapshot) {
-//         if(!snapshot.exists())
-//             return;
-//
-//         // the same countries are stored in this list
-//         let unmatchedRequests = [];
-//         let usersRequests = [];
-//
-//         let users = snapshot.val();
-//         console.log(users);
-//         for(let uid in users){
-//             let user = users[uid];
-//             for(let i=0; i<user.requestCount; i++){
-//                 let request = user.requests["req"+i];
-//                 if(uid == firebase.auth().currentUser.uid)
-//                     usersRequests.push(request);
-//                 else if(request.match != "null")
-//                     unmatchedRequests.push(request);
-//             }
-//         }
-//
-//     });
-// })
-$(function() {
-        // initializeFirebaseApp();
-        // TODO: has to send in the location of the user to the function
-        var countryLocation = "Angola";
-        //TODO: has to get the UID of the customer
-        var uid;
-        unmatchedCountry = [];
-        let firebaseRef = firebase.database().ref();
-        console.log(firebaseRef);
-        firebaseRef.once('value').then(function (snapshot) {
-            let snap = snapshot.val();
-            // i is the userid of the user
-            for (var i in snap) {
-                if(i === uid)
-                {
-                    // user won't be able to get his own request
-                    continue;
-                }
-                for (var j in snap[i]) {
-                    if (j === "requests") {
-                        for (var k in snap[i][j]) {
-                            for(var l in snap[i][j][k])
-                            {
-                                if(l === "destCountry" && snap[i][j][k]["destCountry"] === countryLocation)
-                                {
-                                    unmatchedCountry.push(i+"/"+j+"/"+k+"/");
-                                }
-                                else{
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        })
-    }
-);
+$(function(){
+    initializeFirebaseApp();
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    let reqid = urlParams.get("reqid");
+    $("#display").html("Matching " + reqid);
+});
--- templates/src/js/transactions.js
+++ templates/src/js/transactions.js
@@ -1,1 +1,41 @@
-document.getElementById("transaction").innerHTML = "These are the transactions that you have had"
+$(function(){
+    initializeFirebaseApp();
+
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            displayTransactions();
+        } else {
+            // User is signed out.
+            // ...
+        }
+    });
+});
+
+function displayTransactions(){
+    let uid = firebase.auth().currentUser.uid;
+    let db = firebase.firestore();
+    let ref = db.collection("requests").orderBy("submitDate", "desc").where("user", "==", uid);
+
+    ref.get()
+        .then(function(snapshot) {
+            snapshot.forEach(function(doc){
+                appendRequest(doc.data());
+            });
+        })
+        .catch(function(error){
+            console.log("Error getting documents: ", error);
+        });
+}
+
+function appendRequest(request){
+    $("#table-body").append("" +
+        "            <tr>\n" +
+        "                <th scope=\"row\">" + request.submitDate.toDate().toGMTString() + "</th>\n" +
+        "                <td>" + request.srcCountry + "</td>\n" +
+        "                <td>" + request.destCountry + "</td>\n" +
+        "                <td>" + request.amount + "</td>\n" +
+        "                <td>" + request.flexibility + "</td>\n" +
+        "                <td>" + request.status + "</td>\n" +
+        "                <td>" + request.deadlineDate.toDate().toGMTString()  + "</td>\n" +
+        "            </tr>");
+}
