3d337cf2acafafc6877fc45fe98366d7238beb46
glowies <glowies@visitor096.visitor.dgp.sandbox>
2020-02-17 13:55:50 -0500

    Updated scripts for the User.html page.

--- templates/src/js/user.js
+++ templates/src/js/user.js
@@ -3,17 +3,17 @@ $(function() {
 
     firebase.auth().onAuthStateChanged(function (user) {
         if (user) {
-            var displayName = user.displayName;
-            var email = user.email;
+            let displayName = user.displayName;
+            let email = user.email;
             // could use this to check if the user has logged in successfully and then load his entry in the database
-            var emailVerified = user.emailVerified;
-            var photoURL = user.photoURL;
-            var isAnonymous = user.isAnonymous;
-            var uid = user.uid;
-            var providerData = user.providerData;
+            let emailVerified = user.emailVerified;
+            let photoURL = user.photoURL;
+            let isAnonymous = user.isAnonymous;
+            let uid = user.uid;
+            let providerData = user.providerData;
 
             //TODO: Only save if user doesn't exist
-            saveData(email, displayName, uid);
+            saveUser(email, displayName, uid);
 
             $("#profilePic").attr("src", photoURL);
             $("#usernameHeader").html(displayName);
