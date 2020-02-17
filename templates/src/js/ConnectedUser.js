0647f97b5b46ce61f32f549ffd7c993f45f458e3
adityagoyal <goyaladi>
2020-02-16 23:39:55 -0500

    Fixed bugs

--- templates/src/js/ConnectedUser.js
+++ templates/src/js/ConnectedUser.js
@@ -1,23 +1,23 @@
 $(function(){
     let userId = document.location.search.replace(/^.*?\=/, "");
-    // initializeFirebaseApp();
+
     console.log(userId);
-    // let db = firebase.firestore();
-    // let docRef = db.collection("users").doc(userId);
-    //
-    // docRef.get().then(function(doc) {
-    //     if (doc.exists) {
-    //
-    //         $("#profilePic").attr("src", photoURL);
-    //         $("#usernameHeader").html(displayName);
-    //         $("#userEmail").html(email);
-    //
-    //
-    //     } else {
-    //         // doc.data() will be undefined in this case
-    //         console.log("No such document!");
-    //     }
-    // }).catch(function(error) {
-    //     console.log("Error getting document:", error);
-    // });
+    let db = firebase.firestore();
+    let docRef = db.collection("users").doc(userId);
+
+    docRef.get().then(function(doc) {
+        if (doc.exists) {
+
+            // $("#profilePic").attr("src", doc.data().);
+            $("#usernameHeader").html(doc.data().name);
+            $("#userEmail").html(doc.data().email);
+
+
+        } else {
+            // doc.data() will be undefined in this case
+            console.log("No such document!");
+        }
+    }).catch(function(error) {
+        console.log("Error getting document:", error);
+    });
 });
